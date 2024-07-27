7da45fe55e3c5316dbaf37ad2392e773682ca012
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-07-26 18:30:56 -0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,8 @@
+//This is server/config/db.js
+
 const mongoose = require("mongoose");
 
 mongoose.set('strictQuery', false);
-
 
 mongoose.connect("mongodb://127.0.0.1:27017/BlogApp").then(()=>{
     console.log("connected!");
